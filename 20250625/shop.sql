e7f3a611d27d16796f4afb49dd6149508a3465e2
dlqjsdp <dlqjsdp224@gmail.com>
2025-06-25 18:02:53 +0900

    20250625

---
+++ 20250625/shop.sql
@@ -0,0 +1,23 @@
+use shop;
+
+select * from member;
+
+desc cart;
+
+select * from cart;
+
+desc cart_item;
+
+desc order_item;
+
+-- 상품 확인
+select * from item;
+
+-- 상품이미지 확인
+select * from item_img;
+
+-- 주문 확인
+select * from orders;
+
+-- 주문 상품 확인
+select * from order_item;
